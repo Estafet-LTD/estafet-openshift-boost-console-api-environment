cf60ef3c3e87d5fd706b2eb8d3c869a7ca402696
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-24 20:49:38 +0000

    Fixed the app schema as the former did not allow for multiple stages

--- ddl/drop-console-environment-api-db.ddl
+++ ddl/drop-console-environment-api-db.ddl
@@ -1,3 +1,4 @@
 alter table APP drop constraint APP_TO_ENV_FK;
 drop table if exists APP cascade;
 drop table if exists Env cascade;
+drop sequence APP_ID_SEQ;
--- src/main/java/com/estafet/openshift/boost/console/api/environment/model/App.java
+++ src/main/java/com/estafet/openshift/boost/console/api/environment/model/App.java
@@ -3,9 +3,12 @@ package com.estafet.openshift.boost.console.api.environment.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -18,7 +21,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class App {
 
 	@Id
-	@Column(name = "APP_ID", nullable = false)
+	@SequenceGenerator(name = "APP_ID_SEQ", sequenceName = "APP_ID_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "APP_ID_SEQ")
+	@Column(name = "APP_ID")
+	private Long id;
+	
+	@Column(name = "APP_NAME", nullable = false)
 	private String name;
 
 	@Column(name = "VERSION", nullable = false)
@@ -124,8 +132,7 @@ public class App {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((env == null) ? 0 : env.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -138,15 +145,10 @@ public class App {
 		if (getClass() != obj.getClass())
 			return false;
 		App other = (App) obj;
-		if (env == null) {
-			if (other.env != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!env.equals(other.env))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
